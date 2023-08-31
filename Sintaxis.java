d398d3e57a204dec0010850fb091569b39d97a99
Aleexqnder <jassonsuazo0801@gmail.com>
2023-08-30 19:38:48 -0600

    Sintaxis

---
+++ Sintaxis.java
@@ -0,0 +1,17 @@
+public class Sintaxis {
+    public static void main (String[] args){
+        System.out.println("Mostrar texto en pantalla");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(5 + 5);
+        //Este es un comentario en java
+        /*Este es un comentario de 
+         * siete lineas
+         * 1
+         * 2
+         * 3
+         * 4
+         */
+    }
+}
