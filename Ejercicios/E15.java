7472eb2be3ef1760bbfc14484b35ad3f4c4ce18e
Aleexqnder <jassonsuazo0801@gmail.com>
2023-11-14 23:19:08 -0600

    Added shapes to E15.java

--- Ejercicios/E15.java
+++ Ejercicios/E15.java
@@ -1,10 +1,53 @@
 package Ejercicios;
+
 public class E15 {
     public static void main(String[] args) {
-        System.out.print( "*" );
-        System.out.print( "***" );
-        System.out.print( "*****" );
-        System.out.print( "****" );
-        System.out.println( "**" );
+        System.out.println("Cuadro:");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == 0 || i == 8 || j == 0 || j == 8) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Óvalo:");
+        System.out.println("  ***  ");
+        for (int i = 0; i < 7; i++) {
+            System.out.println("*     *");
+        }
+        System.out.println("  ***  ");
+
+        System.out.println("Flecha:");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        for (int i = 0; i < 6; i++) {
+            System.out.println("    *");
+        }
+
+        System.out.println("Diamante:");
+        System.out.println("         *");
+        System.out.println("        * *");
+        System.out.println("       *   *");
+        System.out.println("      *     *");
+        System.out.println("     *       *");
+        System.out.println("    *         *");
+        System.out.println("   *           *");
+        System.out.println("  *             *");
+        System.out.println(" *               *");
+        System.out.println("*                 *");
+        System.out.println(" *               *");
+        System.out.println("  *             *");
+        System.out.println("   *           *");
+        System.out.println("    *         *");
+        System.out.println("     *       *");
+        System.out.println("      *     *");
+        System.out.println("       *   *");
+        System.out.println("        * *");
+        System.out.println("         *");
     }
 }
