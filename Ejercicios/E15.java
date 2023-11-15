7ba4c58519fd42a1fa75813f9031e79cf63785c9
Aleexqnder <jassonsuazo0801@gmail.com>
2023-11-14 22:48:17 -0600

    Refactor code for improved readability and performance.

---
+++ Ejercicios/E15.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+public class E15 {
+    public static void main(String[] args) {
+        System.out.print( "*" );
+        System.out.print( "***" );
+        System.out.print( "*****" );
+        System.out.print( "****" );
+        System.out.println( "**" );
+    }
+}
